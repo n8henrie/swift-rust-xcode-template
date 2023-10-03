1f2c1beeff4af5b8ae185dbae951091732c65e25
Nathan Henrie <nate@n8henrie.com>
2023-10-03 11:01:43 -0600

    fmt

--- src/lib.rs
+++ src/lib.rs
@@ -1,28 +1,31 @@
-use std::os::raw::{c_char};
-use std::ffi::{CString, CStr};
+use std::ffi::{CStr, CString};
+use std::os::raw::c_char;
 
 #[no_mangle]
-pub extern fn rust_hello(to: *const c_char) -> *mut c_char {
+pub extern "C" fn rust_hello(to: *const c_char) -> *mut c_char {
     let c_str = unsafe { CStr::from_ptr(to) };
     let recipient = match c_str.to_str() {
         Err(_) => "there",
         Ok(string) => string,
     };
-    CString::new("Hello ".to_owned() + recipient).unwrap().into_raw()
+    CString::new("Hello ".to_owned() + recipient)
+        .unwrap()
+        .into_raw()
 }
 
 #[no_mangle]
-pub extern fn rust_hello_free(s: *mut c_char) {
+pub extern "C" fn rust_hello_free(s: *mut c_char) {
     unsafe {
-        if s.is_null() { return }
+        if s.is_null() {
+            return;
+        }
         drop(CString::from_raw(s))
     };
 }
 
 #[no_mangle]
-pub extern fn use_swift_closure(swift_fn: extern "C" fn(i32) -> i32) {
-    let input : i32 = 10;
+pub extern "C" fn use_swift_closure(swift_fn: extern "C" fn(i32) -> i32) {
+    let input: i32 = 10;
     let output = swift_fn(input);
     println!("Swift closure turned {:?} into {:?}", input, output);
-
 }
